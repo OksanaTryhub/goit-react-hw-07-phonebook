25bb584b1447bc0e6422c091dabc295d2428ba5e
OksanaTryhub <ksanatrigub@gmail.com>
2023-03-03 21:53:40 +0200

    update

--- src/components/redux/contacts/contacts-slice.js
+++ src/components/redux/contacts/contacts-slice.js
@@ -10,42 +10,43 @@ const initialState = {
 const contanctsSlice = createSlice({
   name: 'contacts',
   initialState,
-  extraReducers: builder => {
-    builder
-      .addCase(actions.fetchAllContactsLoading, state => {
-        state.loading = true;
-      })
-      .addCase(actions.fetchAllContactsSuccess, (state, { payload }) => {
-        state.loading = false;
-        state.items = payload;
-      })
-      .addCase(actions.fetchAllContactsError, (state, { payload }) => {
-        state.loading = false;
-        state.error = payload;
-      })
-      .addCase(actions.fetchAddContactLoading, state => {
-        state.loading = true;
-      })
-      .addCase(actions.fetchAddContactSuccess, (state, { payload }) => {
-        state.loading = false;
-        state.items.push(payload);
-      })
-      .addCase(actions.fetchAddContactError, (state, { payload }) => {
-        state.loading = false;
-        state.error = payload;
-      })
-      .addCase(actions.fetchDeleteContactLoading, state => {
-        state.loading = true;
-      })
-      .addCase(actions.fetchDeleteContactSuccess, (state, { payload }) => {
-        state.loading = false;
-        const index = state.items.findIndex(item => item.id === payload);
-        state.items.splice(index, 1);
-      })
-      .addCase(actions.fetchDeleteContactError, (state, { payload }) => {
-        state.loading = false;
-        state.error = payload;
-      });
+  extraReducers: {
+    [actions.fetchAllContactsLoading]: state => {
+      state.loading = true;
+    },
+    [actions.fetchAllContactsSuccess]: (state, { payload }) => {
+      state.loading = false;
+      state.items = payload;
+    },
+    [actions.fetchAllContactsError]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+
+    [actions.fetchAddContactLoading]: state => {
+      state.loading = true;
+    },
+    [actions.fetchAddContactSuccess]: (state, { payload }) => {
+      state.loading = false;
+      state.items.push(payload);
+    },
+    [actions.fetchAddContactError]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+
+    [actions.fetchDeleteContactLoading]: state => {
+      state.loading = true;
+    },
+    [actions.fetchDeleteContactSuccess]: (state, { payload }) => {
+      state.loading = false;
+      const index = state.items.findIndex(item => item.id === payload);
+      state.items.splice(index, 1);
+    },
+    [actions.fetchDeleteContactError]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
   },
 });
 
